09964e17d88d39830656ac5c238948985f370d57
Justin Linville <jlinville01@gmail.com>
2017-02-26 21:15:26 -0800

    Update Page_Test.java

--- Tests/Page_Test.java
+++ Tests/Page_Test.java
@@ -15,50 +15,29 @@ import page_objects.StandOut;
 
 public class Page_Test extends StandOut
 {
-	@Before
-	public void setUp() 
-	{
-		super.setUp();
-	}
-	
-	public void initialize(java.util.Properties pr, WebDriver dr, String url, java.sql.Connection db)
-	{
-		super.initialize(pr, dr, url, db);
-	}
-	
 	@Test
 	public void runTest() throws Exception 
 	{
 		// Reset Persona Account
-		DB_Reset db_reset = new DB_Reset(this.driver, this.con);
-		db_reset.resetPersona(props.getProperty("ResetPersonaemail"), props.getProperty("ResetPersonaEndemail"), props.getProperty("defaultclientID"));
+		app.db_reset.resetAccount(props.getProperty("ResetAccountEmail"), props.getProperty("ResetAccountEndEmail"), props.getProperty("defaultClientID"));
 		
 		// Login
-		this.login = new Login();
-		this.login.initialize(this.props, this.driver, this.baseUrl);
-		this.login.login(props.getProperty("defaultTL"), props.getProperty("defaultpassword"));
+		app.login.login(props.getProperty("defaultTL"), props.getProperty("defaultpassword"));
 		
 		// Instantiate TestPage page 
-		TestPage testPage = new TestPage(this.driver);
-		testPage.navigateTestPage(this.driver, this.baseUrl);
-		testPage.onboardingTestPage();
+		app.testPage.navigateTestPage(this.driver, this.baseUrl);
+		app.testPage.onboardingTestPage();
 		
 		// Verify elements and text
-		assertThat(testPage.HTWBWMIsDisplayed(this.driver), is(true));
-		assertThat(testPage.CTMWIsDisplayed(this.driver), is(true));
-		assertThat(testPage.addPostIsDisplayed(this.driver), is(true));
-		assertThat(testPage.postsHeaderIsDisplayed(this.driver), is(true));
-		assertThat(testPage.filterButtonIsDisplayed(this.driver), is(true));
-		assertThat(testPage.myLearningPulseSnapshotTitleText(), is("My Learning Pulse Snapshot"));
-		assertThat(testPage.myFavoritesTitleText(), is("My Favorites"));
+		assertThat(app.testPage.HTWBWMIsDisplayed(this.driver), is(true));
+		assertThat(app.testPage.CTMWIsDisplayed(this.driver), is(true));
+		assertThat(app.testPage.addPostIsDisplayed(this.driver), is(true));
+		assertThat(app.testPage.postsHeaderIsDisplayed(this.driver), is(true));
+		assertThat(app.testPage.filterButtonIsDisplayed(this.driver), is(true));
+		assertThat(app.testPage.myLearningPulseSnapshotTitleText(), is("My Learning Pulse Snapshot"));
+		assertThat(app.testPage.myFavoritesTitleText(), is("My Favorites"));
 		
 		// Logout
-		this.login.logout();
-	}
-	
-	@After
-	public void tearDown()
-	{
-		driver.quit();
+		app.login.logout();
 	}
 }
